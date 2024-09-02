c17492228447dc72477a8e40256db921b9e80b83
Abhimanyu Pal <palabhimanyu@gmail.com>
2024-09-02 20:21:46 +0530

    Added  practice programs for Nested and Switch statement .

---
+++ switchandnested/Leetcode.java
@@ -0,0 +1,63 @@
+package switchandnested;
+
+import java.util.Scanner;
+
+public class Leetcode {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+ //       **Area Of Circle Java Program
+//        System.out.print("Enter the radius of circle");
+//        int radius = input.nextInt();
+//        float area = 3.14f* radius * radius ;
+//        System.out.println("Area:"+area);
+        // ** Subtract the Product and Sum of Digits of an Integer
+//        int sum=0,product=1;
+//        System.out.println("Enter the integer:");
+//        int n=input.nextInt();
+//        while(n>0){
+//            int rem=n%10;
+//            sum=sum+rem;
+//            product=product*rem;
+//            n=n/10;
+//        }
+//        System.out.println(product-sum);
+        //** Factor of the number
+//        System.out.println("Enter the integer ");
+//        int n=input.nextInt();
+//        for (int i = 1; i <= n; ++i) {
+//            if (n % i == 0) {
+//                System.out.print(i + " ");
+//            }
+//        }
+        //** Calculate Electricity Bill
+//        System.out.println("Enter Units Consumed:");
+//        float units = input.nextFloat();
+//        if(units<=100){
+//            System.out.println("Amount:" +units*3);
+//        } else if (units>100) {
+//            System.out.println("Amount:" +(100 * 1.50 + (units - 100) * 2.50));
+//        }
+//        ** Calculate Ncr and Npr.
+//        System.out.println("Enter the value of n;");
+//        int n = input.nextInt();
+//        System.out.println("Enter the value of r:");
+//        int r = input.nextInt();
+//        int resultn = 1;
+//        for (int i = 1; i <= n; i++) {
+//            resultn *= i;
+//        }
+//        int resultr=1;
+//        for(int i=1;i<=r;i++){
+//            resultr*=i;
+//        }
+//        int resultnr=1;
+//        for(int i=1;i<=(n-r);i++){
+//            resultr*=i;
+//        }
+//        int c=resultn /( resultr * resultnr );
+//        int p=resultn / resultnr  ;
+//        System.out.println("Ncr:"+c);
+//        System.out.println("Npr:"+p);
+
+    }
+}
